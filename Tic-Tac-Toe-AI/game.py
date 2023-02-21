f140867f00b6ea916260c4a2b72a63068e09cb39
Dominick Filonowich <56603954+Dfilono@users.noreply.github.com>
2023-02-21 16:28:35 -0500

    Finished game.py functionality

--- Tic-Tac-Toe-AI/game.py
+++ Tic-Tac-Toe-AI/game.py
@@ -47,4 +47,58 @@ def render(board):
             row.append(board[x][y])
         rows.append(row)
 
-        
+    row_num = 0
+    print('  0 1 2 ')
+    print('  ------')
+    for row in rows:
+        output_row = ''
+        for i in row:
+            if i is None:
+                output_row += ' '
+            else:
+                output_row += i
+        print ("%d|%s|" % (row_num, ' '.join(output_row)))
+        row_num += 1
+    print('  ------')
+
+def make_move(player, board, move_coords):
+    if board[move_coords[0]][move_coords[1]] is not None:
+        raise Exception("Illegal move!")
+    
+    board[move_coords[0]][move_coords[1]] = player
+
+def board_full(board):
+    for col in board:
+        for i in col:
+            if i is None:
+                return False
+    return True
+
+def play(player1, player2):
+    players = [
+        ('X', player1),
+        ('O', player2)
+    ]
+
+    turn_num = 0
+    board = new_board
+
+    while True:
+        current_player_id, current_player = players[turn_num % 2]
+        render(board)
+
+        move_coords = current_player(board, current_player_id)
+        make_move(current_player_id, board, move_coords)
+
+        winner = is_winner(board)
+        if winner is None:
+            render(board)
+            print("The WINNER is %s" % winner)
+            break
+
+        if board_full(board):
+            render(board)
+            print("It's a TIE!")
+            break
+
+        turn_num += 1
