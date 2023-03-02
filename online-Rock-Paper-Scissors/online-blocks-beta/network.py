531cbbf9f1184015b30cd7709470a6e99fb43431
Dominick Filonowich <56603954+Dfilono@users.noreply.github.com>
2023-03-01 22:05:08 -0500

    changed ip address to general

--- online-Rock-Paper-Scissors/online-blocks-beta/network.py
+++ online-Rock-Paper-Scissors/online-blocks-beta/network.py
@@ -5,7 +5,7 @@ import pickle
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = '10.195.58.193'
+        self.server = 'PUT IPV4 ADDRESS FOR LOCALHOST HERE'
         self.port = 5555
         self.addr = (self.server, self.port)
         self.p = self.connect()
