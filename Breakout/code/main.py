d663918b3fb553e6af1e1f23dc2f77ffec5ef85c
Dominick Filonowich <56603954+Dfilono@users.noreply.github.com>
2023-03-27 03:16:35 -0400

    Added powerups and lives

--- Breakout/code/main.py
+++ Breakout/code/main.py
@@ -2,9 +2,9 @@ import pygame as pg
 import sys
 import time
 from settings import *
-from sprites import Player, Ball, Block
+from sprites import Player, Ball, Block, PowerUp, Projectile
 from surfacemaker import SurfaceMaker
-from random import choices
+from random import choices, choice
 from string import digits
 
 class Game:
@@ -19,12 +19,26 @@ class Game:
         # sprite groups
         self.all_sprites = pg.sprite.Group()
         self.block_sprites = pg.sprite.Group()
+        self.powerup_sprites = pg.sprite.Group()
+        self.projectile_spirtes = pg.sprite.Group()
 
         # setup
         self.surfacemaker = SurfaceMaker()
         self.player = Player(self.all_sprites, self.surfacemaker)
         self.ball = Ball(self.all_sprites, self.player, self.block_sprites)
         self.stage_setup()
+
+        # lives
+        self.lives_surf = pg.image.load('../graphics/other/heart.png').convert_alpha()
+
+        # projectile
+        self.projectile_surf = pg.image.load('../graphics/other/projectile.png').convert_alpha()
+        self.can_shoot = True
+        self.shoot_time = 0
+
+    def create_powerup(self, pos):
+        powerup_type = choice(POWERUPS)
+        PowerUp(pos, powerup_type, [self.all_sprites, self.powerup_sprites])
 
     def create_bg(self):
         bg_og = pg.image.load('../graphics/other/bg.png').convert()
@@ -52,9 +66,36 @@ class Game:
             for col_idx, col in enumerate(row):
                 if col != ' ':
                     x = col_idx * (BLOCK_WIDTH + GAP_SIZE) + GAP_SIZE // 2
-                    y = row_idx * (BLOCK_HEIGHT + GAP_SIZE) + GAP_SIZE // 2
-                    Block(col, (x, y),[self.all_sprites, self.block_sprites], self.surfacemaker)
+                    y = TOP_OFFSET + row_idx * (BLOCK_HEIGHT + GAP_SIZE) + GAP_SIZE // 2
+                    Block(col, (x, y),[self.all_sprites, self.block_sprites], self.surfacemaker, self.create_powerup)
 
+    def display_lives(self):
+        for i in range(self.player.lives):
+            x = 2 + i * (self.lives_surf.get_width() + 2)
+            self.display_surface.blit(self.lives_surf, (x, 4))
+
+    def powerup_collision(self):
+        overlap_sprites = pg.sprite.spritecollide(self.player, self.powerup_sprites, True)
+
+        for sprite in overlap_sprites:
+            self.player.powerup(sprite.powerup_type)
+
+    def create_projectile(self):
+        for projectile in self.player.laser_rects:
+            Projectile(projectile.midtop - pg.math.Vector2(0, 30), self.projectile_surf, [self.all_sprites, self.projectile_spirtes])
+
+    def projectile_timer(self):
+        if pg.time.get_ticks() - self.shoot_time >= 500:
+            self.can_shoot = True
+
+    def projectile_collisions(self):
+        for projectile in self.projectile_spirtes:
+            overlap_sprites = pg.sprite.spritecollide(projectile, self.block_sprites, False)
+
+            if overlap_sprites:
+                for sprite in overlap_sprites:
+                    sprite.get_damage(1)
+                projectile.kill()
 
     def run(self):
         t = time.time()
@@ -64,7 +105,7 @@ class Game:
             t = time.time()
 
             for event in pg.event.get():
-                if event.type == pg.QUIT:
+                if event.type == pg.QUIT or self.player.lives <= 0 or len(self.block_sprites) == 0:
                     pg.quit()
                     sys.exit()
 
@@ -72,12 +113,27 @@ class Game:
                     if event.key == pg.K_SPACE:
                         self.ball.active = True
 
+                        if self.can_shoot:
+                            self.create_projectile()
+                            self.can_shoot = False
+                            self.shoot_time = pg.time.get_ticks()
+                    
+                    if event.key == pg.K_ESCAPE:
+                        pg.quit()
+                        sys.exit()
+
+            self.display_surface.blit(self.bg, (0, 0))
+            
             # update game
             self.all_sprites.update(dt)
+            self.powerup_collision()
+            self.projectile_timer()
+            self.projectile_collisions()
 
             # draw frame
-            self.display_surface.blit(self.bg, (0, 0))
+            
             self.all_sprites.draw(self.display_surface)
+            self.display_lives()
 
             pg.display.update()
 
--- Breakout/code/sprites.py
+++ Breakout/code/sprites.py
@@ -1,13 +1,14 @@
 import pygame as pg
 from settings import *
-from random import choice
+from random import choice, randint
 
 class Player(pg.sprite.Sprite):
     def __init__(self, groups, surfacemaker):
         super().__init__(groups)
 
         # setup
-        self.suracemaker = surfacemaker
+        self.display_surface = pg.display.get_surface()
+        self.surfacemaker = surfacemaker
         self.image = surfacemaker.get_surf('player', (WINDOW_WIDTH // 10, WINDOW_HEIGHT // 20))
         #self.image.fill('red')
 
@@ -17,6 +18,13 @@ class Player(pg.sprite.Sprite):
         self.direction = pg.math.Vector2()
         self.pos = pg.math.Vector2(self.rect.topleft)
         self.vel = 300
+
+        self.lives = 5
+
+        # laser
+        self.laser_amount = 0
+        self.laser_surf = pg.image.load('../graphics/other/laser.png').convert_alpha()
+        self.laser_rects = []
 
     def controls(self):
         keys = pg.key.get_pressed()
@@ -37,12 +45,42 @@ class Player(pg.sprite.Sprite):
             self.rect.left = 0
             self.pos.x = self.rect.x
 
+    def powerup(self, powerup_type):
+        if powerup_type == 'speed':
+            self.vel += 50
+        
+        if powerup_type == 'heart':
+            self.lives += 1
+
+        if powerup_type == 'size':
+            new_w = self.rect.width * 1.1
+            self.image = self.surfacemaker.get_surf('player', (new_w, self.rect.height))
+            self.rect = self.image.get_rect(center = self.rect.center)
+            self.pos.x = self.rect.x
+
+        if powerup_type == 'laser':
+            self.laser_amount += 1
+
+    def display_laser(self):
+        self.laser_rects = []
+        if self.laser_amount > 0:
+            divider_length = self.rect.width / (self.laser_amount + 1)
+
+            for i in range(self.laser_amount):
+                x = self.rect.left + divider_length * (i + 1)
+                laser_rect = self.laser_surf.get_rect(midbottom = (x, self.rect.top))
+                self.laser_rects.append(laser_rect)
+
+            for laser_rect in self.laser_rects:
+                self.display_surface.blit(self.laser_surf, laser_rect)
+
     def update(self, dt):
         self.old_rect = self.rect.copy()
         self.controls()
         self.pos.x += self.direction.x * self.vel * dt
         self.rect.x = round(self.pos.x)
         self.screen_bounds()
+        self.display_laser()
 
 class Ball(pg.sprite.Sprite):
     def __init__(self, groups, player, blocks):
@@ -123,6 +161,7 @@ class Ball(pg.sprite.Sprite):
             if self.rect.bottom > WINDOW_HEIGHT:
                 self.active = False
                 self.direction.y = -1
+                self.player.lives -= 1
 
     def update(self, dt):
         if self.active:
@@ -146,7 +185,7 @@ class Ball(pg.sprite.Sprite):
             self.pos = pg.math.Vector2(self.rect.topleft)
 
 class Block(pg.sprite.Sprite):
-    def __init__(self, block_type, pos, groups, surfacemaker):
+    def __init__(self, block_type, pos, groups, surfacemaker, create_powerup):
         super().__init__(groups)
         
         self.surfacemaker = surfacemaker
@@ -157,10 +196,49 @@ class Block(pg.sprite.Sprite):
         # damage
         self.health = int(block_type)
 
+        # power ups
+        self.create_powerup = create_powerup
+        self.prob = 30
+
     def get_damage(self, amount):
         self.health -= amount
 
         if self.health > 0:
             self.image = self.surfacemaker.get_surf(COLOR_LEGEND[str(self.health)], (BLOCK_WIDTH, BLOCK_HEIGHT))
         else:
+            if randint(1, 100) >= self.prob:
+                self.create_powerup(self.rect.center)
+            self.kill()
+
+class PowerUp(pg.sprite.Sprite):
+    def __init__(self, pos, powerup_type, groups):
+        super().__init__(groups)
+        self.powerup_type = powerup_type
+        self.image = pg.image.load(f'../graphics/upgrades/{powerup_type}.png').convert_alpha()
+        self.rect = self.image.get_rect(midtop = pos)
+
+        self.pos = pg.math.Vector2(self.rect.topleft)
+        self.vel = 300
+
+    def update(self, dt):
+        self.pos.y += self.vel * dt
+        self.rect.y = round(self.pos.y)
+
+        if self.rect.top > WINDOW_HEIGHT + 100:
+            self.kill()
+
+class Projectile(pg.sprite.Sprite):
+    def __init__(self, pos, surf, groups):
+        super().__init__(groups)
+        self.image = surf
+        self.rect = self.image.get_rect(midbottom = pos)
+
+        self.pos = pg.math. Vector2(self.rect.topleft)
+        self.vel = 300
+
+    def update(self, dt):
+        self.pos.y -= self.vel * dt
+        self.rect.y = round(self.pos.y)
+
+        if self.rect.bottom <= -100:
             self.kill()
